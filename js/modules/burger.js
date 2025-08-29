657640654c20b1ebc282f87d60844445d690f246
killrealpp <kaysarovkirill5@gmail.com>
2025-08-30 00:34:46 +0300

    добавил доработки

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -3,19 +3,36 @@ export function initBurger() {
     const menu = document.querySelector('.menu')
     const menuItems = document.querySelectorAll('.menu__item')
     const menuNumber = document.querySelector('.menu__link-number')
+    const overlay = document.querySelector('.overlay')
 
-    if (!btn || !menu) return
+    if (!btn || !menu || !overlay) return
+
+    function openMenu() {
+        menu.classList.add('open-menu')
+        btn.classList.add('active')
+        overlay.classList.add('active')
+        document.body.classList.add('lock')
+    }
+
+    function closeMenu() {
+        menu.classList.remove('open-menu')
+        btn.classList.remove('active')
+        overlay.classList.remove('active')
+        document.body.classList.remove('lock')
+    }
 
     btn.addEventListener('click', () => {
-        menu.classList.toggle('open-menu')
-        btn.classList.toggle('active')
+        if (menu.classList.contains('open-menu')) {
+            closeMenu()
+        } else {
+            openMenu()
+        }
     })
 
     if (menuItems) {
         menuItems.forEach((item) => {
             item.addEventListener('click', () => {
-                menu.classList.remove("open-menu");
-                btn.classList.remove("active");
+                closeMenu()
                 menuNumber.style.display = 'block'
             })
         })
@@ -24,9 +41,10 @@ export function initBurger() {
     document.addEventListener('click', (e) => {
         const isClickInside = menu.contains(e.target) || btn.contains(e.target)
         if (!isClickInside && menu.classList.contains("open-menu")) {
-            menu.classList.remove("open-menu");
-            btn.classList.remove("active");
+            closeMenu()
             menuNumber.style.display = 'none'
         }
     })
+
+    overlay.addEventListener('click', closeMenu)
 }
